b9dec99590e457fafa6bccd721148aa52c146c01
jjgomeza22 <jjgomeza22@gmail.com>
2025-09-01 17:49:38 -0500

    HU-3 fixing unit tests of signUpNewUserUseCase

--- domain/usecase/src/test/java/co/com/crediya/usecase/signupnewuser/SignUpNewUserUseCaseTest.java
+++ domain/usecase/src/test/java/co/com/crediya/usecase/signupnewuser/SignUpNewUserUseCaseTest.java
@@ -1,5 +1,7 @@
 package co.com.crediya.usecase.signupnewuser;
 
+import co.com.crediya.model.password.gateways.PasswordEncoderPort;
+import co.com.crediya.model.role.RoleName;
 import co.com.crediya.model.user.User;
 import co.com.crediya.model.user.gateways.UserRepository;
 import co.com.crediya.usecase.exception.DuplicateEmailException;
@@ -23,21 +25,26 @@ class SignUpNewUserUseCaseTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    PasswordEncoderPort passwordEncoder;
+
     private final User user = User.builder()
             .name("juan")
             .lastname("gomez")
             .email("juan@mail.com")
             .documentId("10947832854")
             .phoneNumber("3146576454")
-            .roleId(1)
+            .role(RoleName.ADMIN)
             .baseSalary(new BigDecimal(8500000))
             .birthDay(LocalDate.of(1999, 5, 22))
             .address("Las Mercedes")
+            .password("123456")
             .build();
 
     @Test
     void shouldSignUpNewUser() {
         Mockito.when(userRepository.finByEmail(Mockito.anyString())).thenReturn(Mono.empty());
+        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn("$2a$10$ZsLC0s.81fSiLjwJPTn01Onz0.s01ZKq.P32dDQJM6YaMPacklbCu");
         Mockito.when(userRepository.saveUser(Mockito.any(User.class))).thenReturn(Mono.just("OK"));
 
         signUpNewUserUseCase.execute(user)
@@ -49,7 +56,6 @@ class SignUpNewUserUseCaseTest {
     @Test
     void shouldReturnDuplicateEmailException() {
         Mockito.when(userRepository.finByEmail(Mockito.anyString())).thenReturn(Mono.just(user));
-        Mockito.when(userRepository.saveUser(Mockito.any(User.class))).thenReturn(Mono.just("OK"));
 
         signUpNewUserUseCase.execute(user)
                 .as(StepVerifier::create)
